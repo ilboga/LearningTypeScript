61dced98a916181874778e861f17323b4e8bfe4d
Michael Bogani <michael.bogani@promemoriagroup.com>
2026-02-16 07:30:32 +0100

    object and unions narrow trail

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,49 @@
 export function runCommands() {
 	// Declare your variables and runtime logic here! ✨
+	type AvailableResources = "food" | "water" | undefined;
+	type Food = number;
+	type Water = number;
+	type Day = number;
+
+	let day: Day = 1;
+	let food: Food = 5;
+	let water: Water = 5;
+	let available: AvailableResources = undefined;
+
+	while (day <= 7) {
+		day++;
+		let dice: number = Math.floor(Math.random() * 6) + 1;
+		switch (dice) {
+			case 1:
+				available = "food";
+				break;
+			case 2:
+				available = "water";
+				break;
+			default:
+				switch (available) {
+					case undefined:
+						available = dice % 2 === 0 ? "food" : "water";
+						break;
+					case "food":
+						food += dice;
+						available = undefined;
+						break;
+					case "water":
+						water += dice;
+						available = undefined;
+						break;
+				}
+				break;
+		}
+
+		food--;
+		water--;
+
+		if (!food || !water) {
+			return false;
+		}
+	}
+
+	return true;
 }
