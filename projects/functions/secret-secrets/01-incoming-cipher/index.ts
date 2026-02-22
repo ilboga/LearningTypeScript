2bdb6f8030a2026de70dfa018ebb6bd336fbf052
Michael Bogani <michael.bogani@promemoriagroup.com>
2026-02-22 09:33:20 +0100

    function secrets secrets

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,18 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type stringToString = (text: string) => string;
+
+export function createCipher(cypher: stringToString): stringToString {
+	let output = (text: string) => {
+		let tornaQuesto = "";
+		for (const char of text) {
+			tornaQuesto += cypher(char);
+		}
+
+		return tornaQuesto;
+	};
+	return output;
+}
+
+// createCipher((e) => "abc")
