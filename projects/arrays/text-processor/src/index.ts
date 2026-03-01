48e9538cfa5d57b0d5124c47062b4789858a2654
Michael Bogani <michael.bogani@promemoriagroup.com>
2026-03-01 20:41:21 +0100

    array: attempt

--- projects/arrays/text-processor/src/index.ts
+++ projects/arrays/text-processor/src/index.ts
@@ -1,2 +1,55 @@
 // Write your alignTexts function here! ✨
 // You'll need to export it so the tests can run it.
+
+type AlignType = "left" | "middle" | "right";
+
+function padder(
+	word: string,
+	align: AlignType = "left",
+	paddedNtimes: number,
+): string {
+	// qui faccio il pad
+	switch (align) {
+		case "right":
+			word = " " + word;
+			break;
+		case "middle":
+			word = paddedNtimes % 2 ? " " + word : word + " ";
+			break;
+		default:
+			word = word + " ";
+			break;
+	}
+	return word;
+}
+
+export function alignTexts(
+	texts: string[],
+	options: {
+		align?: AlignType;
+		width: number;
+	},
+): string[][] {
+	let splittedAr: string[][] = [];
+	for (let i = 0; i < texts.length; i++) {
+		let words: string[] = texts[i].split(" ");
+		splittedAr[i] = [];
+
+		for (let k = 0; k < words.length; k++) {
+			let paddedNtimes = 0;
+			let word = words[k];
+
+			word = padder(word, options.align, paddedNtimes);
+
+			while (word.length < options.width) {
+				paddedNtimes++;
+				console.log(paddedNtimes, word);
+				// qui faccio il pad
+				word = padder(word, options.align, paddedNtimes);
+			}
+			splittedAr[i][k] = word;
+		}
+	}
+
+	return splittedAr;
+}
